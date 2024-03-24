cd584949d1604d0d5ef8128749ab7e3fde0a289d
Uschi <dolfus@zbh.uni-hamburg.de>
2024-03-24 20:55:32 +0100

    after make of first migrations

--- legrandproject/lists/models.py
+++ legrandproject/lists/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class TodoList(models.Model):
+    title = models.CharField(max_length=200)
+
+class TodoItem(models.Model):
+    text = models.CharField(max_length=200)
+    done = models.BooleanField()
+    edit_date = models.DateTimeField('edit date')
+    parent_list = models.ForeignKey(TodoList, on_delete=models.CASCADE)
